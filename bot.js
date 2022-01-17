0e16d0c9bc5342cf43a4b336ddd12961955b89e4
kk <705555262@qq.com>
2022-01-17 23:03:15 +0800

    remove liquidity check, remove delay buy, add chinese log

--- bot.js
+++ bot.js
@@ -8,25 +8,21 @@ const app = express();
 dotenv.config();
 
 const data = {
-  BNB: process.env.BNB_CONTRACT, //bnb
+  BNB: process.env.BNB_CONTRACT, // BNB 合约地址
+  to_PURCHASE: process.env.TO_PURCHASE, // 目标合约地址
+  AMOUNT_OF_BNB : process.env.AMOUNT_OF_BNB, // 购买的BNB数量
 
-  to_PURCHASE: process.env.TO_PURCHASE, // token that you will purchase = BUSD for test '0xe9e7cea3dedca5984780bafc599bd69add087d56'
-
-  AMOUNT_OF_BNB : process.env.AMOUNT_OF_BNB, // how much you want to buy in BNB
-
-  factory: process.env.FACTORY,  //PancakeSwap V2 factory
-
-  router: process.env.ROUTER, //PancakeSwap V2 router
+  factory: process.env.FACTORY,  // PancakeSwap V2 factory
+  router: process.env.ROUTER, // PancakeSwap V2 router
 
   recipient: process.env.YOUR_ADDRESS, //your wallet address,
 
-  Slippage : process.env.SLIPPAGE, //in Percentage
+  Slippage : process.env.SLIPPAGE, // 滑点
 
-  gasPrice : ethers.utils.parseUnits(`${process.env.GWEI}`, 'gwei'), //in gwei
+  gasPrice : ethers.utils.parseUnits(`${process.env.GWEI}`, 'gwei'), // 单位gwei
+  gasLimit : process.env.GAS_LIMIT, // 至少 21000
 
-  gasLimit : process.env.GAS_LIMIT, //at least 21000
-
-  minBnb : process.env.MIN_LIQUIDITY_ADDED //min liquidity added
+  minBnb : process.env.MIN_LIQUIDITY_ADDED // 流动性池子最小值，若流动性小于设定则停止购买，默认0.1
 }
 
 let initialLiquidityDetected = false;
@@ -74,27 +70,30 @@ const run = async () => {
 }
 
   let checkLiq = async() => {
-    const pairAddressx = await factory.getPair(tokenIn, tokenOut);
-    console.log(chalk.blue(`pairAddress: ${pairAddressx}`));
-    if (pairAddressx !== null && pairAddressx !== undefined) {
-      // console.log("pairAddress.toString().indexOf('0x0000000000000')", pairAddress.toString().indexOf('0x0000000000000'));
-      if (pairAddressx.toString().indexOf('0x0000000000000') > -1) {
-        console.log(chalk.cyan(`pairAddress ${pairAddressx} not detected. Auto restart`));
-        return await run();
-      }
-    }
-    const pairBNBvalue = await erc.balanceOf(pairAddressx);
-    jmlBnb = await ethers.utils.formatEther(pairBNBvalue);
-    console.log(`value BNB : ${jmlBnb}`);
+    // const pairAddressx = await factory.getPair(tokenIn, tokenOut);
+    // console.log(chalk.blue(`流动性检测成功，交易对: ${pairAddressx}`));
+    // if (pairAddressx !== null && pairAddressx !== undefined) {
+    //   // console.log("pairAddress.toString().indexOf('0x0000000000000')", pairAddress.toString().indexOf('0x0000000000000'));
+    //   if (pairAddressx.toString().indexOf('0x0000000000000') > -1) {
+    //     console.log(chalk.blue(`流动性：${pairAddressx}未检测成功，自动重启中`));
+    //     return await run();
+    //   }
+    // }
+    // const pairBNBvalue = await erc.balanceOf(pairAddressx);
+    // jmlBnb = await ethers.utils.formatEther(pairBNBvalue);
+    // console.log(`流动性: ${jmlBnb} BNB`);
+    //
+    // if(parseFloat(jmlBnb) > parseFloat(data.minBnb)){
+    //     setTimeout(() => buyAction(), 1);
+    // }
+    // else{
+    //     initialLiquidityDetected = false;
+    //     console.log(' run again...');
+    //     return await run();
+    //   }
 
-    if(parseFloat(jmlBnb) > parseFloat(data.minBnb)){
-        setTimeout(() => buyAction(), 3000);
-    }
-    else{
-        initialLiquidityDetected = false;
-        console.log(' run again...');
-        return await run();
-      }
+    console.log(chalk.red("跳过流动性检测"))
+    setTimeout(() => buyAction(), 1);
   }
 
   let buyAction = async() => {
@@ -103,7 +102,6 @@ const run = async () => {
         return null;
     }
 
-    console.log('ready to buy');
     try{
       initialLiquidityDetected = true;
 
@@ -116,30 +114,21 @@ const run = async () => {
         amountOutMin = amounts[1].sub(amounts[1].div(`${data.Slippage}`))
       }
 
-      console.log(
-       chalk.green.inverse(`Start to buy \n`)
-        +
-        `Buying Token
-        =================
-        tokenIn: ${(amountIn * 1e-18).toString()} ${tokenIn} (BNB)
-        tokenOut: ${amountOutMin.toString()} ${tokenOut}
-      `);
-
-      console.log('Processing Transaction.....');
-      console.log(chalk.yellow(`amountIn: ${(amountIn * 1e-18)} ${tokenIn} (BNB)`));
-      console.log(chalk.yellow(`amountOutMin: ${amountOutMin}`));
-      console.log(chalk.yellow(`tokenIn: ${tokenIn}`));
-      console.log(chalk.yellow(`tokenOut: ${tokenOut}`));
-      console.log(chalk.yellow(`data.recipient: ${data.recipient}`));
-      console.log(chalk.yellow(`data.gasLimit: ${data.gasLimit}`));
-      console.log(chalk.yellow(`data.gasPrice: ${data.gasPrice}`));
+      console.log('交易处理中......');
+      console.log('============================================================');
+      console.log(chalk.green(`BNB数量: ${(amountIn * 1e-18).toString()} BNB`))
+      console.log(chalk.green(`目标Token: ${amountOutMin} (合约地址：${tokenOut})`))
+      console.log(chalk.green(`滑点: ${data.Slippage}%`));
+      console.log(chalk.green(`gasLimit: ${data.gasLimit}`));
+      console.log(chalk.green(`gasPrice: ${data.gasPrice / 1000000000} GWEI`));
+      console.log('============================================================');
 
       // const tx = await router.swapExactTokensForTokensSupportingFeeOnTransferTokens( //uncomment this if you want to buy deflationary token
       const tx = await router.swapExactETHForTokens( //uncomment here if you want to buy token
         amountOutMin,
         [tokenIn, tokenOut],
         data.recipient,
-        Date.now() + 1000 * 60 * 5, //5 minutes
+        Date.now() + 1000 * 60, // 1 minutes
         {
           'gasLimit': data.gasLimit,
           'gasPrice': data.gasPrice,
@@ -148,29 +137,26 @@ const run = async () => {
       });
 
       const receipt = await tx.wait();
-      console.log(`Transaction receipt : https://www.bscscan.com/tx/${receipt.logs[1].transactionHash}`);
+      console.log(`交易成功，请前往bscscan查看: https://www.bscscan.com/tx/${receipt.logs[1].transactionHash}`);
       setTimeout(() => {process.exit()},2000);
     }catch(err){
       let error = JSON.parse(JSON.stringify(err));
-        console.log(`Error caused by : 
-        {
-        reason : ${error.reason},
-        transactionHash : ${error.transactionHash}
-        message : ${error}
-        }`);
-        console.log(error);
+        console.log(chalk.red("交易发生错误"))
+        console.log(chalk.red(`原因：${error.reason}`))
+        console.log(chalk.red(`交易哈希：${error.transactionHash}`))
+        console.log(chalk.red(`信息：${error.code}`))
 
         inquirer.prompt([
     {
       type: 'confirm',
       name: 'runAgain',
-      message: 'Do you want to run again thi bot?',
+      message: '是否重启？',
     },
   ])
   .then(answers => {
     if(answers.runAgain === true){
       console.log('= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =');
-      console.log('Run again');
+      console.log('重启');
       console.log('= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =');
       initialLiquidityDetected = false;
       run();
@@ -187,4 +173,4 @@ run();
 
 const PORT = 5001;
 
-app.listen(PORT, console.log(chalk.yellow(`Listening for Liquidity Addition to token ${data.to_PURCHASE}`)));
+app.listen(PORT, console.log(chalk.blue(`提供的合约地址为：${data.to_PURCHASE}`)));
